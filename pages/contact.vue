3ef987933ca01645e68914966abd98dfd62fc839
ET-SAPRK <samuelwoyesso2016@gmail.com>
2023-12-29 12:22:15 -0800

    edited

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
   <div class="z-50">
     <div class="text-4xl text-center mt-10 font-bold">Contact Us</div>
     <div class="flex-1 xl:flex lg:flex p-8 justify-center">
-      <div class="bg-gray-800 text-white h-fit p-8">
+      <div class="bg-gray-800 text-white h-auto p-8">
         <div class="flex-1 ml-4 mt-4 items-center">
           <div class="text-4xl font-semibold mt-8 mb-4">Info</div>
           <div class="flex-1">
@@ -85,126 +85,34 @@
         </div>
       </div>
       <div class="text-gray-800">
-        <div class="flex-1 ml-8 items-center">
-          <div class="text-4xl font-semibold mt-4 mb-4 text-center">
+        <div class="flex-1 ml-2 items-center">
+          <div class="text-4xl font-semibold text-center">
             Send us a Message
           </div>
           <div>
-            <form class="mt-20">
-              <div class="flex-1 justify-center xl:flex lg:flex mb-8">
-                <div class="mb-4 ml-3">
-                  <label
-                    for="name"
-                    class="block text-sm font-medium text-gray-700"
-                    >Your Name</label
-                  >
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    class="mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
-                    placeholder="Enter your name"
-                    required
-                  />
+            <form>
+              <div class="grid gap-4 py-4">
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="name" class="text-right"> Your Name </Label>
+                  <Input id="name" type="text" class="col-span-3" />
                 </div>
-
-                <div class="mb-4 ml-3">
-                  <label
-                    for="fatherName"
-                    class="block text-sm font-medium text-gray-700"
-                    >Father Name</label
-                  >
-                  <input
-                    type="text"
-                    id="fatherName"
-                    name="fatherName"
-                    class="mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
-                    placeholder="Enter your father name"
-                    required
-                  />
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="fatherName" class="text-right">
+                    Father Name
+                  </Label>
+                  <Input id="fatherName" type="text" class="col-span-3" />
                 </div>
-              </div>
-
-              <div class="flex-1 justify-center xl:flex lg:flex my-4">
-                <!-- Phone Number Section -->
-                <div class="mb-4 ml-3">
-                  <label
-                    for="phoneNumber"
-                    class="block text-sm font-medium text-gray-700"
-                    >Phone Number</label
-                  >
-                  <div class="relative">
-                    <Icon
-                      name="material-symbols:smartphone"
-                      class="absolute inset-y-0 left-0 pl-2 mt-2 text-gray-500 text-3xl"
-                    />
-                    <input
-                      type="tel"
-                      id="phoneNumber"
-                      name="phoneNumber"
-                      class="pl-10 mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
-                      placeholder="Enter your phone number"
-                      required
-                    />
-                  </div>
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="number" class="text-right">Phone Number </Label>
+                  <Input id="number" type="number" class="col-span-3" />
                 </div>
-
-                <!-- Email Section -->
-                <div class="mb-4 ml-3">
-                  <label
-                    for="email"
-                    class="block text-sm font-medium text-gray-700"
-                    >Email</label
-                  >
-                  <div class="relative">
-                    <!-- Replace this with the appropriate icon for email -->
-                    <Icon
-                      name="material-symbols:alternate-email"
-                      class="absolute inset-y-0 left-0 pl-2 mt-2 text-gray-500 text-3xl"
-                    />
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      class="pl-10 mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
-                      placeholder="Enter your email address"
-                      required
-                    />
-                  </div>
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="email" class="text-right">Email </Label>
+                  <Input id="email" type="email" class="col-span-3" />
                 </div>
-              </div>
-
-              <!-- Message Section -->
-              <div class="mb-4 ml-3">
-                <label
-                  for="message"
-                  class="block text-sm font-medium text-gray-700"
-                  >Message</label
-                >
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  class="mt-1 p-2 border rounded-md w-[250px] lg:w-[520px] xl:w-[520px] md:w-[620px]"
-                  placeholder="Enter your message"
-                  required
-                ></textarea>
-              </div>
-
-              <div class="flex-col justify-start">
-                <div class="mb-4">
-                  <label class="inline-flex items-center">
-                    <input
-                      type="checkbox"
-                      class="form-checkbox h-5 w-5 text-blue-500 items-start"
-                    />
-                    <span class="ml-2 text-gray-700 text-start"
-                      >I accept the terms and conditions</span
-                    >
-                  </label>
-                </div>
-                <div>
-                  <Button type="submit" class="mb-2"> Register </Button>
+                <div class="grid w-full gap-2">
+                  <Textarea placeholder="Type your message here." />
+                  <Button type="submit">Send message</Button>
                 </div>
               </div>
             </form>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,114 @@
   <!-- <Guestfooter /> -->
   <!-- <AuthHeader /> -->
   <!-- <Authfooter /> -->
+
+  <form class="mt-20">
+    <div class="flex-1 justify-center xl:flex lg:flex mb-8">
+      <div class="mb-4 ml-3">
+        <label for="name" class="block text-sm font-medium text-gray-700"
+          >Your Name</label
+        >
+        <input
+          type="text"
+          id="name"
+          name="name"
+          class="mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
+          placeholder="Enter your name"
+          required
+        />
+      </div>
+
+      <div class="mb-4 ml-3">
+        <label for="fatherName" class="block text-sm font-medium text-gray-700"
+          >Father Name</label
+        >
+        <input
+          type="text"
+          id="fatherName"
+          name="fatherName"
+          class="mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
+          placeholder="Enter your father name"
+          required
+        />
+      </div>
+    </div>
+
+    <div class="flex-1 justify-center xl:flex lg:flex my-4">
+      <!-- Phone Number Section -->
+      <div class="mb-4 ml-3">
+        <label for="phoneNumber" class="block text-sm font-medium text-gray-700"
+          >Phone Number</label
+        >
+        <div class="relative">
+          <Icon
+            name="material-symbols:smartphone"
+            class="absolute inset-y-0 left-0 pl-2 mt-2 text-gray-500 text-3xl"
+          />
+          <input
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            class="pl-10 mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
+            placeholder="Enter your phone number"
+            required
+          />
+        </div>
+      </div>
+
+      <!-- Email Section -->
+      <div class="mb-4 ml-3">
+        <label for="email" class="block text-sm font-medium text-gray-700"
+          >Email</label
+        >
+        <div class="relative">
+          <!-- Replace this with the appropriate icon for email -->
+          <Icon
+            name="material-symbols:alternate-email"
+            class="absolute inset-y-0 left-0 pl-2 mt-2 text-gray-500 text-3xl"
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            class="pl-10 mt-1 p-2 border rounded-md w-64 lg:w-64 xl:w-64 md:w-[620px]"
+            placeholder="Enter your email address"
+            required
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- Message Section -->
+    <div class="mb-4 ml-3">
+      <label for="message" class="block text-sm font-medium text-gray-700"
+        >Message</label
+      >
+      <textarea
+        id="message"
+        name="message"
+        rows="4"
+        class="mt-1 p-2 border rounded-md w-[250px] lg:w-[520px] xl:w-[520px] md:w-[620px]"
+        placeholder="Enter your message"
+        required
+      ></textarea>
+    </div>
+
+    <div class="flex-col justify-start">
+      <div class="mb-4">
+        <label class="inline-flex items-center">
+          <input
+            type="checkbox"
+            class="form-checkbox h-5 w-5 text-blue-500 items-start"
+          />
+          <span class="ml-2 text-gray-700 text-start"
+            >I accept the terms and conditions</span
+          >
+        </label>
+      </div>
+      <div>
+        <Button type="submit" class="mb-2"> Register </Button>
+      </div>
+    </div>
+  </form>
 </template>
 <script></script>
