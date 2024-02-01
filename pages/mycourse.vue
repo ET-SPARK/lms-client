0229736110d4d31f3a135bf3e1aadcdfd7d979eb
ET-SAPRK <samuelwoyesso2016@gmail.com>
2024-01-31 22:17:44 -0800

    edited

--- pages/mycourse.vue
+++ pages/mycourse.vue
@@ -1,5 +1,17 @@
 <template>
   <HeaderAuth />
-
+  <div class="border m-24 max-[640px]:m-8 text-center">
+    <div class="text-2xl max-[640px]:text-xl font-bold mt-20">
+      Welcome to Andinet LMS!
+    </div>
+    <div class="font-light mt-10">
+      It looks like you're ready to start on your learning journey. However, it
+      seems this space is empty right now. Explore our course catalog and start
+      learning.
+    </div>
+    <div class="mt-20 mb-10">
+      <Button>Explore Courses</Button>
+    </div>
+  </div>
   <FooterAuth />
 </template>
